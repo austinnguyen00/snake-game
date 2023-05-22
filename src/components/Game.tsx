fe477fa5b2312b330a977f8521d6c27d76b176a8
Austin <ndkhiem3010@gmail.com>
2023-05-22 20:30:16 +0800

    Initializing game variables and states + Updating handleGesture function

--- src/components/Game.tsx
+++ src/components/Game.tsx
@@ -1,29 +1,49 @@
 import { View, Text, SafeAreaView, StyleSheet } from 'react-native';
-import React from 'react';
 import { Colors } from '../styles/colors';
 import { PanGestureHandler } from 'react-native-gesture-handler';
-import { GestureEventType } from '../types/types';
+import { Coordinate, Direction, GestureEventType } from '../types/types';
+import { useState } from 'react';
 
-const handleGesutre = (event: GestureEventType) => {
-	const { translationX, translationY } = event.nativeEvent;
-	if (Math.abs(translationX) > Math.abs(translationY)) {
-		// Moving right
-		if (translationX > 0) {
-		}
-		// Moving left
-		else {
-		}
-	} else {
-		// Moving up
-		if (translationY > 0) {
-		}
-		// Moving down
-		else {
-		}
-	}
-};
+// Constant variables for the game
+const SNAKE_INITIAL_POSITION = [{ x: 5, y: 5 }];
+const FOOD_INITIAL_POSITION = { x: 5, y: 20 };
+const GAME_BOUNDS = { xMin: 0, xMax: 35, yMin: 0, yMax: 63 };
+const MOVE_INTERVAL = 50;
+const SCORE_INCREMENT = 10;
 
 const Game = () => {
+	const [direction, setDirection] = useState<Direction>(Direction.Right);
+	// Keep track of each part of the snake
+	const [snake, setSnake] = useState<Coordinate[]>(SNAKE_INITIAL_POSITION);
+	const [food, setFood] = useState<Coordinate>(FOOD_INITIAL_POSITION);
+	const [score, setScore] = useState<number>(0);
+	const [isGameOver, setIsGameOver] = useState<boolean>(false);
+	const [isPaused, setIsPaused] = useState<boolean>(false);
+
+	// Function to handle snake movement in the game
+	const handleGesutre = (event: GestureEventType) => {
+		const { translationX, translationY } = event.nativeEvent;
+		if (Math.abs(translationX) > Math.abs(translationY)) {
+			// Moving right
+			if (translationX > 0) {
+				setDirection(Direction.Right);
+			}
+			// Moving left
+			else {
+				setDirection(Direction.Left);
+			}
+		} else {
+			// Moving up
+			if (translationY > 0) {
+				setDirection(Direction.Up);
+			}
+			// Moving down
+			else {
+				setDirection(Direction.Down);
+			}
+		}
+	};
+
 	return (
 		<PanGestureHandler onGestureEvent={handleGesutre}>
 			<SafeAreaView style={styles.container}>
