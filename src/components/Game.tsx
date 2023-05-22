231d9df852621946a68fd1cce29129ae23234acd
Austin <ndkhiem3010@gmail.com>
2023-05-22 20:19:08 +0800

    Adding PanGestureHandler with types to our Game component

--- src/components/Game.tsx
+++ src/components/Game.tsx
@@ -1,12 +1,35 @@
 import { View, Text, SafeAreaView, StyleSheet } from 'react-native';
 import React from 'react';
 import { Colors } from '../styles/colors';
+import { PanGestureHandler } from 'react-native-gesture-handler';
+import { GestureEventType } from '../types/types';
+
+const handleGesutre = (event: GestureEventType) => {
+	const { translationX, translationY } = event.nativeEvent;
+	if (Math.abs(translationX) > Math.abs(translationY)) {
+		// Moving right
+		if (translationX > 0) {
+		}
+		// Moving left
+		else {
+		}
+	} else {
+		// Moving up
+		if (translationY > 0) {
+		}
+		// Moving down
+		else {
+		}
+	}
+};
 
 const Game = () => {
 	return (
-		<SafeAreaView style={styles.container}>
-			<Text>Game</Text>
-		</SafeAreaView>
+		<PanGestureHandler onGestureEvent={handleGesutre}>
+			<SafeAreaView style={styles.container}>
+				<Text>Game</Text>
+			</SafeAreaView>
+		</PanGestureHandler>
 	);
 };
 
